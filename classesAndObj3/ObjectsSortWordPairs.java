78c7bb3bddf173c7b1ead0da8611bb0f14804b9a
Bishwas Ghimire <144313610+bishwasghimire22@users.noreply.github.com>
2024-08-25 11:40:50 +0300

    Update ObjectsSortWordPairs.java

--- classesAndObj3/ObjectsSortWordPairs.java
+++ classesAndObj3/ObjectsSortWordPairs.java
@@ -40,3 +40,19 @@ public class ObjectsSortWordPairs {
 	}
 
 }
+/*Create a program called ObjectsSortWordPairs that first inputs pairs of English and Finnish words
+from the user until he/she enters "OK" as the English word.
+Next, the program should sort the list of WordPairs in alphabetical order by English word and print
+the words as shown in the example output.
+Finally, the program should sort the list of WordPairs in alphabetical order by Finnish word and print
+the words as shown in the example output.
+INSTRUCTION ( To be followed carefully! )
+Reuse the WordPair class from a previous exercise.
+• This time, you have to add something to the WordPair class to make WordPair objects
+comparable. Please study the course materials to see how you can sort objects.
+• After the modification, the WordPair class should still work properly with all the other programs
+that use it.
+• Create exactly one list of WordPairs. Do not create any other arrays or lists.
+• Do not use lambda expressions in this exercise.
+Hint: To speed up coding, you can also copy/paste some code from your
+ObjectsTranslationArrayList program.*/
