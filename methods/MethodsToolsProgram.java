bb62c921a8a2aa51ff77c82b951edf686756f669
Bishwas Ghimire <144313610+bishwasghimire22@users.noreply.github.com>
2024-08-25 11:56:02 +0300

    Update MethodsToolsProgram.java

--- methods/MethodsToolsProgram.java
+++ methods/MethodsToolsProgram.java
@@ -25,3 +25,11 @@ public class MethodsToolsProgram {
 	}
 
 }
+/*Create a class called MethodsTools for storing generic methods. That is, the MethodsTools class is a
+method library class, and it should not contain the main method. Copy your already existing code of
+the minimum method from the MethodsMinimum class to the MethodsTools class and do the
+minor modification to make the method accessible from outside of the MethodsTools class.
+Then create a program called MethodsToolsProgram that uses the method minimum of the
+MethodsTools class. The main method should input three integers from the user and call the
+minimum method of the MethodsTools class to determine the minimum of the inputted values.
+Finally, the main method should print the minimum value.*/
