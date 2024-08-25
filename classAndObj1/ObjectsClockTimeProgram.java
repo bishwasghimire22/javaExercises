0730fe9d4e465e2a7a8815dc97ba33805918b65e
Bishwas Ghimire <144313610+bishwasghimire22@users.noreply.github.com>
2024-08-25 11:49:37 +0300

    Update ObjectsClockTimeProgram.java

--- classAndObj1/ObjectsClockTimeProgram.java
+++ classAndObj1/ObjectsClockTimeProgram.java
@@ -27,3 +27,14 @@ public class ObjectsClockTimeProgram {
 	}
 
 }
+/*This time, we want to represent robust clock times. In the real world, a valid clock time is between
+00:00 and 23:59
+Create a new class called ClockTime that represents clock time.
+The additional business rules are the following:
+• Initially, the clock time is always 00:00
+• The add method rolls the time forward by the given number of hours and minutes. If either
+parameter value is negative, then the time should not change.
+• The toString method returns the textual representation of the clock time in the "hh:mm" format
+Hint: You can use if statements to determine if you need to add a leading zeros.
+NB! Please use the ready-made program class below to test your ClockTime class. Do not modify
+the program class at all.*/
