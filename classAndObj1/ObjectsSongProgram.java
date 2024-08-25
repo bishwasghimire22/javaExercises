d2ae908e8d90a204deefe89a538a0966ae793fb7
Bishwas Ghimire <144313610+bishwasghimire22@users.noreply.github.com>
2024-08-25 11:50:29 +0300

    Update ObjectsSongProgram.java

--- classAndObj1/ObjectsSongProgram.java
+++ classAndObj1/ObjectsSongProgram.java
@@ -13,3 +13,12 @@ public class ObjectsSongProgram {
 	}
 
 }
+/*First, create a new class called Song as described in the diagram below. Do not add any other
+features to the Song class. Note: The Song class should not contain any code for inputting data from
+the user or printing data.
+Then, create a main program called ObjectsSongProgram. In the program, create three objects from
+the Song class and assign their references to reference variables. See the example output for the
+song names, artists and years. NB! There should be exactly three variables in the main program. Not
+less, not more. This time, do not use an array or ArrayList.
+Finally, the program should print a song list as shown in the example output. In this exercise, use the
+toString method to get data from each Song object to be printed.*/
